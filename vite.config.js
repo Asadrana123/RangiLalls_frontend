abc802920ae6d3ba00e8459843b391b3b38fa60f
Asad Rana <asadlukman246@gmail.com>
2025-02-18 15:45:03 +0530

    edits

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  define: {
+    'import.meta.env.VITE_RECAPTCHA_SITE_KEY': JSON.stringify(process.env.VITE_RECAPTCHA_SITE_KEY)
+  },
   plugins: [react()],
   base: '/',
   server: {
