b24bcfe0a165c4bf4af501ece24f665acd2518c9
Asad Rana <asadlukman246@gmail.com>
2025-02-26 21:15:28 +0530

    Re-tracking vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  define: {
-    'import.meta.env.VITE_RECAPTCHA_SITE_KEY': JSON.stringify(process.env.VITE_RECAPTCHA_SITE_KEY)
-  },
-  plugins: [react()],
-  base: '/',
-  server: {
-    historyApiFallback: true, // Ensures React Router handles routing
-  },
-  build: {
-    outDir: 'dist', // Ensure correct build output
-  }
+export default defineConfig(({ mode }) => {
+  return {
+    define: mode === 'production'
+      ? {
+          'import.meta.env.VITE_RECAPTCHA_SITE_KEY': JSON.stringify(process.env.VITE_RECAPTCHA_SITE_KEY),
+          'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
+        }
+      : {}, // No need to manually define for development
+    plugins: [react()],
+    base: '/',
+    server: {
+      historyApiFallback: true,
+    },
+    build: {
+      outDir: 'dist',
+    }
+  };
 });
